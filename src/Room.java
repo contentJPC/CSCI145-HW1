6b827a4d5c18b00a062d7fdd2ed6b797a5b1deb4
michaeljgeiger <46984362+michaeljgeiger@users.noreply.github.com>
2019-02-08 18:59:28 -0800

    2.8.19
    added instances of player/monster classes and implemented more of the enter method with attack/run

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,3 @@
-//package com.company;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,29 +16,52 @@ public class Room {
         SetVisited( true);
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+
         Monster monster = new Monster();
         String monsterType = monster.getMonsterType();
         int monsterDamage = monster.getDamage();
         int monsterHealth = monster.getHealth();
+
+        Player player = new Player(1, 1, 1, "Unset", 1);
+        String playerClass = player.getPlayerClass();
+        int playerHealth = player.getHealth();
+        int playerDamage = player.getDamage();
+        int playerGold = player.getGold();
+        double playerLootModifier = player.getLootModifieretLootModifier();
+
         boolean lootOrMonster = random.nextBoolean();
-        if (lootOrMonster) {
-            //monster attack
-            System.out.println("You just encountered a" + monsterType);
-            System.out.println("A " + monsterType + " has " + monsterHealth + " health and does " + monsterDamage + " damage.");
-            System.out.print("Press 1 to attack or 2 to run.");
-            int attackOrRun = scanner.nextInt();
-            if (attackOrRun == 1) {
-                System.out.println("You've chosen to attack");
+        while (monsterHealth > 0) {
+            if (lootOrMonster) {
+                //monster attack
+                System.out.println("You just encountered a" + monsterType);
+                System.out.print(" that has " + monsterHealth + " health and does " + monsterDamage + " damage.");
+                System.out.println();
+                System.out.print("Press 1 to attack or 2 to run.");
+                int attackOrRun = scanner.nextInt();
+
+                if (attackOrRun == 1) {
+                        System.out.println("You've chosen to attack");
+                        playerHealth = playerHealth - monsterDamage;
+                        System.out.println("The " + monsterType + " did " + monsterDamage);
+                        monsterHealth = monsterHealth - playerDamage;
+                        System.out.println("You did " + playerDamage + " damage to the " + monster);
+                }
+
+                else if (attackOrRun == 2) {
+                    playerHealth = playerHealth - monsterDamage;
+                    System.out.println("You've chosen to run");
+                    break;
+                }
             }
+
             else {
+                //loot
 
             }
-        }
-        else {
-            //loot
-
         }
     }
+
+
 
     public boolean Visited() {
         return GetVisited();
